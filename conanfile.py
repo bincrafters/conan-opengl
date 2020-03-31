dc868f8fc07486a03dcddfadbd6f0e77b05b7bff
Croydon <git@cr0ydon.com>
2020-03-31 13:45:42 +0200

    On macOS we only support provider=system

--- conanfile.py
+++ conanfile.py
@@ -12,13 +12,11 @@ class OpenGLConan(ConanFile):
     homepage = "https://opengl.org"
     license = "None"  # TODO: Relax hooks about license attribute for virtual packages? How?
 
-    # TODO: Create virtual package for glu - or add here? Creating it separetly probably prevent options conflict
-    # TODO: What about OpenGL ES?
+    # TODO: Create virtual package for glu
+    # TODO: Create virtual package for OpenGL ES
+    # TODO: Create virtual package for EGL
     # TODO: What about OpenCL?
-    # TODO: What about EGL?
     # TODO: Add check if system_libs are installed if provider=system?
-    # TODO: Try to install system_libs? Probably not
-    # TODO: macOS support for < 10.13 (official OpenGL support) and >= 10.13 (no official OpenGL support)
     # TODO: Write a test_package
 
     settings = {"os"}
@@ -32,18 +30,22 @@ class OpenGLConan(ConanFile):
     def configure(self):
         if self.settings.os == "Windows" and self.options.provider != "system":
             # While we could just raise an error, this would make the consumption of this package much harder
-            # And since the entire idea of this package is to abstract way OpenGL support
+            # And since the entire idea of this package is to abstract away OpenGL support
             # it is probably better to force the value of the option
             self.output.warning("On Windows only opengl:provider=system is supported! Forcing option")
             self.options.provider = "system"
 
+        if self.settings.os == "Macos" and self.options.provider != "system":
+            self.output.warning("On macOS only opengl:provider=system is supported! Forcing option")
+            self.options.provider = "system"
+
 
     def system_requirements(self):
-        # This is really, really bad. Is there any better solution to continue support OpenGL on Apple?
-        if self.settings.os == "Macos" and tools.os_info.is_macos:
-            self.run("brew cask install xquartz")
-
         if self.options.provider == "system":
+            if self.settings.os == "Macos" and tools.os_info.is_macos and tools.Version(self.settings.os.version) >= "10.13":
+                # macOS < 10.13 has native OpenGL support and doesn't need to install xquartz for support
+                # This is really, really bad. Is there any better solution to continue support OpenGL on Apple?
+                self.run("brew cask install xquartz")
             # Note: If you want to disable installation on your system
             # set CONAN_SYSREQUIRES_MODE to disabled
             if self.settings.os == "Linux" and tools.os_info.is_linux:
